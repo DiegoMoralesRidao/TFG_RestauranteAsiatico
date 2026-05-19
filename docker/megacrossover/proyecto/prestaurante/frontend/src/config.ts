f9fd1f083e86407197289ce1a679b0753a4f938c
DiegoMoralesRidao <dmorrid282@g.educaand.es>
2026-05-19 23:19:07 +0200

    Update frontend API_URL configurations to use protocol-relative URL to prevent Mixed Content errors on HTTPS

--- docker/megacrossover/proyecto/prestaurante/frontend/src/config.ts
+++ docker/megacrossover/proyecto/prestaurante/frontend/src/config.ts
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://backend.api.recuperacion.asiaticohuercal.es";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "//backend.api.recuperacion.asiaticohuercal.es";
