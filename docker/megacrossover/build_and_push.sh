7b4f61114e590da1bc8a41c7c9f5bb0d6050d0ee
DiegoMoralesRidao <dmorrid282@g.educaand.es>
2026-05-19 18:23:10 +0200

    Añadir restaurante_frontend01 a build_and_push.sh

--- docker/megacrossover/build_and_push.sh
+++ docker/megacrossover/build_and_push.sh
@@ -9,6 +9,7 @@ docker build -t dmorrid/ubseguridad01 -f dockerfiles/ubseguridad/jhlwubseguridad
 docker build -t dmorrid/nginx101 -f dockerfiles/sweb/nginx/jhlwnginx .
 docker build -t dmorrid/postgre01 -f dockerfiles/postgre/jhlwpostgre .
 docker build -t dmorrid/nginxpokeapi -f dockerfiles/nginxrestaurante/jhlwnginxrestaurante .
+docker build -t dmorrid/restaurante_frontend01 -f dockerfiles/nginxrestaurante/jhlwnginxrestaurante .
 docker build -t dmorrid/restaurante_backend01 -f dockerfiles/nest/jhlwrestaurante .
 
 echo "Subiendo imagenes a Docker Hub..."
@@ -17,6 +18,8 @@ docker push dmorrid/ubseguridad01
 docker push dmorrid/nginx101
 docker push dmorrid/postgre01
 docker push dmorrid/nginxpokeapi
+docker push dmorrid/restaurante_frontend01
 docker push dmorrid/restaurante_backend01
 
 echo "¡Todo listo!"
+
